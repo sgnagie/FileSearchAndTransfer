c04077030682a3e4dec0b667469c4724a948bd69
sgnagie <sgnagie@hotmail.com>
2017-12-17 12:09:01 -0500

    committing project

---
+++ JournalFileTransfer/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace JournalFileTransfer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btBeginImport = new System.Windows.Forms.Button();
+            this.lbMessage = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(331, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(30, 30);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(30, 54);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(301, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Import File";
+            // 
+            // btBeginImport
+            // 
+            this.btBeginImport.Location = new System.Drawing.Point(146, 199);
+            this.btBeginImport.Name = "btBeginImport";
+            this.btBeginImport.Size = new System.Drawing.Size(108, 33);
+            this.btBeginImport.TabIndex = 3;
+            this.btBeginImport.Text = "Start";
+            this.btBeginImport.UseVisualStyleBackColor = true;
+            this.btBeginImport.Click += new System.EventHandler(this.btBeginImport_Click);
+            // 
+            // lbMessage
+            // 
+            this.lbMessage.AutoSize = true;
+            this.lbMessage.Location = new System.Drawing.Point(106, 199);
+            this.lbMessage.Name = "lbMessage";
+            this.lbMessage.Size = new System.Drawing.Size(0, 13);
+            this.lbMessage.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(30, 157);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(300, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(331, 150);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(30, 30);
+            this.button2.TabIndex = 6;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Save Destination";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(30, 104);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(300, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(331, 97);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(30, 30);
+            this.button3.TabIndex = 9;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Search Directory";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.ClientSize = new System.Drawing.Size(400, 242);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.lbMessage);
+            this.Controls.Add(this.btBeginImport);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "File Transfer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btBeginImport;
+        private System.Windows.Forms.Label lbMessage;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ JournalFileTransfer/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+using System.Linq;
+
+namespace JournalFileTransfer
+{
+    public partial class Form1 : Form
+    {
+        string file = "";                                       //input file in csv format with no headers
+        string saveDest = "";                                   //Dir we will be saving too
+        string SearchDir = "";                                  //Dir we will be searching
+        List<string> filesToFind = new List<string>();          //list of files we will be searching for including both journal ID an payment ID
+        List<string> paymentFilesList = new List<string>();     //list to compare to to make sure all files are found
+        List<string> foundFilesList = new List<string>();       //list of all found files
+
+        public Form1()
+        {
+            InitializeComponent();
+            SetMyButtonIcon();
+            //textBox2.Text = "D:\\temp";
+            //textBox3.Text = "D:\\temp";
+            saveDest = textBox2.Text;
+            SearchDir = textBox3.Text;
+        }
+
+        private void SetMyButtonIcon()
+        {
+            // Assign an image to the button.
+            Image img = Properties.Resources.folder_32x32;
+            button1.Image = img;
+            button2.Image = img;
+            button3.Image = img;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Displays an OpenFileDialog so the user can select a Cursor.  
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "CSV Files | *.csv";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.Title = "Select a Cursor File";
+
+            // Show the Dialog.  
+            // If the user clicked OK in the dialog  
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                // Assign the cursor in the Stream to the Form's Cursor property.  
+                textBox1.Text = openFileDialog1.FileName;
+            }
+        }
+
+        //sets the save destination where all folders will be created and files saved
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    textBox2.Text = fbd.SelectedPath;
+                    saveDest = textBox2.Text;
+                }
+            }
+        }
+
+        // Sets the search directory and all subfolders
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    textBox3.Text = fbd.SelectedPath;
+                    SearchDir = textBox3.Text;
+                }
+            }
+        }
+
+        private void btBeginImport_Click(object sender, EventArgs e)
+        {
+            
+            if(String.IsNullOrEmpty(textBox1.Text) | String.IsNullOrEmpty(textBox2.Text) | String.IsNullOrEmpty(textBox3.Text))
+            {
+                MessageBox.Show("Please select an Import file, Save Destination and Search Directory.");
+            }
+            else
+            {
+                file = textBox1.Text;
+                ParseFile(file);
+                CreateFoldersAndCopyFiles(filesToFind);
+            }
+        }
+
+        private List<string> ParseFile(string file)
+        {
+            try
+            {
+                using (var reader = new StreamReader(file))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        var values = line.Split(';');
+                        filesToFind.Add(values[0]);
+                    }
+                }
+            }
+            catch (IOException)
+            {
+                MessageBox.Show("Unable to access file. It may be in use by another process.");
+            }
+            return filesToFind;
+        }
+
+        // Method to check for folders existance and create if not. 
+        private void CreateFoldersAndCopyFiles(List<string> listA)
+        {
+            foreach(string t in listA)
+            {
+                string[] val = t.Split(',');
+                string journalID = val[0];
+                string paymentID = val[1];
+                string SaveFilePath = saveDest + "\\" + journalID;
+                paymentFilesList.Add(paymentID);
+                //next create folder to save to in saveDest
+                try
+                {
+                    if (!Directory.Exists(SaveFilePath))
+                    {
+                        Directory.CreateDirectory(SaveFilePath);
+                    }
+
+                    SearchFilesAndCopy(paymentID, SearchDir, SaveFilePath);
+
+                }
+                catch (Exception ex)
+                {
+                    // handle them here
+                    lbMessage.Text = "Unable to create and save to this destination \r\n" + ex.Message;
+                    lbMessage.Refresh();
+                }
+
+            }
+
+            List<string> FilesNotFound = new List<string>();
+
+                    foreach (var str in paymentFilesList)
+                    {
+                        // FirstOrDefault finds first match or returns default (null for string) if not found.
+                        var match = foundFilesList.FirstOrDefault(s => s.StartsWith(str));
+                        if (match == null)
+                        {
+                            FilesNotFound.Add(str);
+                        }
+                    }
+
+            if(FilesNotFound.Count > 0)
+            {
+                TextWriter tw = new StreamWriter(saveDest + "\\FilesNotFound.txt");
+                foreach (String s in FilesNotFound)
+                    tw.WriteLine(s);
+                tw.Close();
+                MessageBox.Show(FilesNotFound.Count + " files were not found.\n\r " + "Check the \"FilesNotFound.txt\" file for this list:");
+            }
+
+            MessageBox.Show("Complete!");
+            CloseProgram();
+        }
+
+        private void SearchFilesAndCopy(string paymentID, string SearchDir, string SaveDir)
+        {
+            string partialName = paymentID;
+            DirectoryInfo hdDirectoryInWhichToSearch = new DirectoryInfo(SearchDir);
+            FileInfo[] filesInDir = hdDirectoryInWhichToSearch.GetFiles("*" + partialName + "*.*", SearchOption.AllDirectories);
+
+            foreach (FileInfo foundFile in filesInDir)
+            {
+                lbMessage.Text = "Found and copying file - " + foundFile;
+                lbMessage.Refresh();
+                foundFilesList.Add(Path.GetFileNameWithoutExtension(foundFile.ToString()));
+                string fullName = foundFile.FullName;
+                string toDest = SaveDir + "\\" + foundFile;
+                File.Copy(fullName, toDest, true);
+                System.Threading.Thread.Sleep(250);
+            }
+        }
+
+        private void CloseProgram()
+        {
+            this.Close();
+            Application.Exit();
+        }
+
+        private void openFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+    }
+}
